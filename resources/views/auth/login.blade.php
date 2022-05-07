c2da53d37390d7c8783dfd98190df031962e3366
Mohammed <56185049+MH-fre20@users.noreply.github.com>
2022-05-07 14:51:03 +0300

    fully responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-center" >
         <div class="col-md-8">
             <div class="card" style="background-color: rgb(90, 90, 90); color: white;">
-                <div class="card-header" dir="rtl"><span class="span">داخل بوون</span></div>
+                <div class="card-header" ><span class="span">داخل بوون</span></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-                        <div class="row mb-3" dir="rtl">
-                            <label for="email" class="col-md-4 col-form-label text-md-end" style="margin-left: -22px"><span class="span">ئیمێڵ</span></label>
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end" style="margin-left: -2px"><span class="span">ئیمێڵ</span></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -24,8 +24,8 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3" dir="rtl">
-                            <label for="password" class="col-md-4 col-form-label text-md-end" style="margin-left: -22px"><span class="span">ژمارەی نهێنی</span></label>
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end" style="margin-left: -2px"><span class="span">ژمارەی نهێنی</span></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -38,7 +38,7 @@
                             </div>
                         </div>
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-1">
+                            <div class="col-md-8 offset-md-8">
                                 <button type="submit" class="btn btn-primary">
                                     <span class="span">داخل بوون</span>
                                 </button>
